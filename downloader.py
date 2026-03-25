ae9ebf769623af486eff9c6ca094747b8242c3b1
0i8c <m.a.-9@hotmail.com>
2026-03-25 03:02:55 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -6,22 +6,13 @@ class UniversalDownloader:
         self.download_path = download_path
         os.makedirs(download_path, exist_ok=True)
 
-    def download(self, url, mode="video"):
-        if mode == "audio":
-            ydl_opts = {
-                'format': 'bestaudio',
-                'outtmpl': f'{self.download_path}/%(title)s.%(ext)s',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                }],
-            }
-        else:
-            ydl_opts = {
-                'format': 'bestvideo+bestaudio/best',
-                'outtmpl': f'{self.download_path}/%(title)s.%(ext)s',
-                'merge_output_format': 'mp4',
-            }
+    def download(self, url):
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': f'{self.download_path}/%(title)s.%(ext)s',
+            'quiet': True,
+            'no_warnings': True,
+        }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
